52352b681c995df9c995c4dfeb17825bfef1633a
miguelonn <64271211+miguelonn@users.noreply.github.com>
2021-10-11 16:54:42 -0300

    cambio pequeño de la barra chocosoft

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,9 +53,11 @@
                                 </li>
                             @endif
                         @else
+                            @if (Auth::user()->rol=='Administrador')
                             <li class="nav-item">
                                 <a class="nav-link" href="/agregarcarrera">Crear Carrera</a>
                             </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
