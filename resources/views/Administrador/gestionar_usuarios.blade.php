6ed6e87998f08c54df3213a82af781c028ad2ca6
juanox44 <juan.avila@alumnos.ucn.cl>
2021-10-13 00:03:48 -0300

    probando el merge

--- resources/views/Administrador/gestionar_usuarios.blade.php
+++ resources/views/Administrador/gestionar_usuarios.blade.php
@@ -7,11 +7,8 @@
 <h1>Aqui se administran los usuarios</h1>
 
 <center><a href="{{ route('crearusuario') }}"><button class="btn btn-primary" type="button">Crear Usuario</button></a></center>
-<<<<<<< HEAD
 <br>
 <center><a href="{{ route('home') }}"><button class="btn btn-primary" type="button">Volver Menu</button></a></center>
-=======
->>>>>>> parent of e9a56d8 (Merge branch 'matiRama' into juanRama)
 
 
 @else
