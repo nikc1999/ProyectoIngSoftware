0e557fb6a2e91c1ab03bc292df80193ff6380df8
Nicolas Sanchez <38233141+nikc1999@users.noreply.github.com>
2021-11-17 19:19:08 -0300

    aa

--- routes/web.php
+++ routes/web.php
@@ -58,9 +58,6 @@ Route::get('/mostrarsolicitudespendientesjefe', [App\Http\Controllers\UserContro
 Route::get('/mostrarsolicitudesdfiltrar', [App\Http\Controllers\UserController::class, 'mostrarSolicitudesFiltrar'])->name('mostrarSolicitudesFiltrar');
 
 Route::post('/buscarestudiante/buscar', [App\Http\Controllers\BuscarEstudiante::class, 'show'])->name('buscarEstudiante');
-<<<<<<< HEAD
-
 
 Route::get('/infoSolicitud', [App\Http\Controllers\UserController::class, 'mostrarInfoSolicitudBoton'])->name('mostrarInfoSolicitudBoton');
-=======
->>>>>>> parent of bd88ab9 (casi terminado buscar estudiante)
+
