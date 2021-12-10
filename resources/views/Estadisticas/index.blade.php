92e3e8f1f4c73f1ca48f6a53012f8574a3865596
miguelonn <64271211+miguelonn@users.noreply.github.com>
2021-12-09 21:21:14 -0300

    Creacion de estadisticas uwu

---
+++ resources/views/Estadisticas/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <form method="GET" action="{{route('filtrarEstadistica')}}">
+
+        <input id="fecha_inicio" name="fecha_inicio" value= "{{old('fecha_inicio')}}" type="date">
+        <input id="fecha_fin" name="fecha_fin" value= "{{old('fecha_fin')}}" type="date">
+
+        <button style="color:white; background-color:rgb(188,97,36)" class="btn">Filtrar Fecha</button>
+
+    </form>
+
+
+    <h1 style="font-size: 50px" class="text-center">Estadísticas del sistema</h1>
+    <div class="row row-cols-1 row-cols-md-2">
+        <div class="col mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div id="chartContainerTipo" style="width: 100%; height: 300px;"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div id="chartContainerStatus" style="height: 300px; width: 100%;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var chart = new CanvasJS.Chart("chartContainerTipo", {
+    animationEnabled: true,
+    theme: "light1", // "light1", "light2", "dark1", "dark2"
+    title:{
+    text: "Solicitudes por tipo"
+    },
+    axisY: {
+    title: "Cantidad de solicitudes"
+    },
+    data: [{
+    type: "column",
+    showInLegend: false,
+    legendMarkerColor: "grey",
+    legendText: "MMbbl = one million barrels",
+    dataPoints: [
+    { y: JSON.parse("{{json_encode($cantTipoSobrecupo)}}"), label: "Sobrecupo" },
+    { y: JSON.parse("{{json_encode($cantTipoCambioParalelo)}}"), label: "Cambio" },
+    { y: JSON.parse("{{json_encode($cantTipoEliminarAsignatura)}}"), label: "Eliminación" },
+    { y: JSON.parse("{{json_encode($cantTipoInscripcionAsignatura)}}"), label: "Inscripción" },
+    { y: JSON.parse("{{json_encode($cantTipoAyudantia)}}"), label: "Ayudantía" },
+    { y: JSON.parse("{{json_encode($cantTipoFacilidad)}}"), label: "Facilidades" },
+    ]
+    }]
+    });
+    var chart2 = new CanvasJS.Chart("chartContainerStatus", {
+    animationEnabled: true,
+    title:{
+    text: "Solicitudes por estado",
+    horizontalAlign: "center"
+    },
+    data: [{
+    type: "doughnut",
+    startAngle: 60,
+    innerRadius: 50,
+    indexLabelFontSize: 12,
+    indexLabel: "{label} - #percent%",
+    toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+    dataPoints: [
+    { y: JSON.parse("{{json_encode($totalPendiente)}}"), label: "Pendiente" },
+    { y: JSON.parse("{{json_encode($totalRechazada)}}"), label: "Rechazada" },
+    { y: JSON.parse("{{json_encode($totalAceptada)}}"), label: "Aceptada" },
+    { y: JSON.parse("{{json_encode($totalAceptadaObs)}}"), label: "Aceptada con obs." },
+    { y: JSON.parse("{{json_encode($totalAnulada)}}"), label: "Anulada" },
+    ]
+    }]
+    });
+    chart.render();
+    chart2.render();
+</script>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
